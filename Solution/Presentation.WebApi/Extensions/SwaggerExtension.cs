602f87420c6483f6419a0e10a81b9c7a59da2f73
Leandro Andrade <eusouleoandrade@hotmail.com>
2022-09-08 00:21:43 -0300

    Adiciona as entidades base e de implementação.

---
+++ Solution/Presentation.WebApi/Extensions/SwaggerExtension.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Presentation.WebApi.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static void AddSwaggerExtension(this IServiceCollection services)
+        {
+            if (services == null)
+                throw new ArgumentNullException(nameof(services));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Todo List v1",
+                    Description = "Your todo list v1."
+                });
+            });
+        }
+
+        public static void UseSwaggerExtension(this IApplicationBuilder app)
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo List - V1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
--- Solution/Presentation.WebApi/Program.cs
+++ Solution/Presentation.WebApi/Program.cs
@@ -1,25 +1,18 @@
+
+// Configure services
+using Presentation.WebApi.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerExtension();
 
+// Configure app
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwaggerExtension();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
