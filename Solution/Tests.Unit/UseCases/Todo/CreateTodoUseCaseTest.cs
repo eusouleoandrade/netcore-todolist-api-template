0f7dce0c76cf412127938168b6aa2fd3670c2e16
Leandro Andrade <eusouleoandrade@hotmail.com>
2022-09-25 02:39:23 -0300

    Implementa o teste de unidade para o CreateTodoUseCase.

---
+++ Solution/Tests.Unit/UseCases/Todo/CreateTodoUseCaseTest.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Core.Application.Dtos.Requests;
+using Core.Application.Interfaces.Repositories;
+using Core.Application.Mappings;
+using Core.Application.UseCases;
+using FluentAssertions;
+using Infra.Notification.Contexts;
+using Xunit;
+
+namespace Tests.Unit.UseCases.Todo
+{
+    public class CreateTodoUseCaseTest
+    {
+        private readonly IMapper _mapperMock;
+        private readonly Moq.Mock<ITodoRepositoryAsync> _todoRepositoryAsyncMock;
+
+        public CreateTodoUseCaseTest()
+        {
+            // Mock repository
+            _todoRepositoryAsyncMock = new Moq.Mock<ITodoRepositoryAsync>();
+
+            // Set auto mapper configs
+            var mapperConfigurationMock = new MapperConfiguration(cfg => cfg.AddProfile(new GeneralProfile()));
+            _mapperMock = mapperConfigurationMock.CreateMapper();
+        }
+
+        [Theory(DisplayName = "Should Run Successfully")]
+        [InlineData("Ir ao mercado.")]
+        [InlineData("Ir ao Dentista.")]
+        [InlineData("Fazer investimentos.")]
+        [InlineData("Pagar as contas.")]
+        public async Task ShouldExecuteSucessfullyAsync(string title)
+        {
+            // Arranje
+            var notificationContext = new NotificationContext();
+            var createTodoUseCase = new CreateTodoUseCase(notificationContext, _mapperMock, _todoRepositoryAsyncMock.Object);
+            var useCaseRequest = new CreateTodoUseCaseRequest(title);
+
+            // Act
+            var useCaseResponse = await createTodoUseCase.RunAsync(useCaseRequest);
+
+            // Assert
+            useCaseResponse.Should().BeNull();
+        }
+    }
+}
