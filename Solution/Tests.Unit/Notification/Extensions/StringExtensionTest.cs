97b027a0db7105d1ce2ce7ea869691ae23c7e3c4
Leandro Andrade <eusouleoandrade@gmail.com>
2022-12-11 20:00:30 -0300

    Adiciona testes para o notification.

---
+++ Solution/Tests.Unit/Notification/Extensions/StringExtensionTest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tests.Unit.Notification.Extensions
+{
+    public class StringExtensionTest
+    {
+        
+    }
+}
